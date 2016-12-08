72c46e258a99b3bd01e7bff8e04e7f23df691d38
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-12-08 19:40:11 +0300

    Add auto generated images and names

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,12 @@
 import mongoose, { Schema } from 'mongoose';
+import names from 'starwars-names';
 
 export const UserSchema = new Schema({
   email: String,
   password: String,
+  fullName: { type: String, default: names.random },
+  photoUrl: { type: String, default: () => `https://github.com/identicons/${new Date().getMilliseconds()}.png` },
+  status: { type: String, default: 'online' },
 });
 
 export default mongoose.model('User', UserSchema);
