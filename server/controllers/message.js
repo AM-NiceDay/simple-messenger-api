494e6e9363c9c0e98bbf6cdbdcef2ce69c976246
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-12-08 17:39:53 +0300

    Implement web sockets to the client and pub/sub between servers

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -1,5 +1,9 @@
+import co from 'co';
+import redis from 'redis';
 import Message from '../models/Message';
-import Chat from '../models/Message';
+import Chat from '../models/Chat';
+
+const redisPub = redis.createClient();
 
 export const getChatMessages = (req, res) => {
   const userId = req.user._id;
@@ -9,23 +13,28 @@ export const getChatMessages = (req, res) => {
     .then(messages => res.status(200).json(messages));
 };
 
-export const createChatMessage = (req, res) => {
+export const createChatMessage = co.wrap(function* (req, res) {
+  const redisClient = req.app.get('redis-client');
+
   const userId = req.user._id;
   const { chatId } = req.params;
   const { text } = req.body;
 
-  const message = new Message({
+  const message = yield new Message({
     chatId,
     userId,
     text,
-  });
+  }).save();
 
-  message.save()
-    .then(message => {
-      return Chat.findByIdAndUpdate(chatId, {
-        lastMessageId: message.id,
-      })
-        .then(() => message);
-    })
-    .then(message => res.status(200).json(message));
-};
+  const chat = yield Chat.findByIdAndUpdate(chatId, { $set: { lastMessageId: message.id }});
+
+  res.status(200).json(message);
+
+  const redisMessage = {
+    toUserId: chat.userIds.filter(id => id !== userId)[0],
+    messageId: message.id,
+    chatId: chatId,
+  };
+
+  redisPub.publish('r/new-message', JSON.stringify(redisMessage));
+});
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import http from 'http';
+import socket from 'socket.io';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import redis from 'redis';
+import co from 'co';
 import config from '../config';
-import routes from './routes'
+import routes from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,4 +21,38 @@ app.use(morgan('dev'));
 
 app.use('/api/v1/', routes);
 
-module.exports = app;
+
+const redisSub = redis.createClient();
+const server = http.Server(app);
+const io = socket(server);
+
+const getNewMessage = (clientUserId) => {
+  return new Promise((resolve) => {
+    redisSub.on('message', (channel, message) => {
+      const newMessage = JSON.parse(message);
+      console.log(`${clientUserId}: message for ${newMessage.toUserId}`);
+      if (channel === 'r/new-message' && clientUserId === newMessage.toUserId) {
+        resolve(newMessage);
+      }
+    });
+  });
+}
+
+io.on('connection', (socket) => {
+  socket.on('ws/listening', co.wrap(function* ({ userId }) {
+    console.log(`Client ${userId} listening for incomming messages`);
+    while (true) {
+      const newMessage = yield getNewMessage(userId);
+      console.log(`New message for client ${userId}: ${newMessage.messageId}`);
+      socket.emit('ws/new-message', newMessage);
+    }
+  }));
+});
+
+redisSub.on('error', function (err) {
+  console.log('Error:', err);
+});
+
+redisSub.subscribe('r/new-message');
+
+module.exports = server;
